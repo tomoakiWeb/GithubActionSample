88d7187827280896c179ad9805e88b56865bedf6
tomoakiWeb <tomomiura0026@gmail.com>
2025-02-01 23:03:46 +0900

    Create project

---
+++ GithubActionSample/GithubActionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionSampleApp.swift
+//  GithubActionSample
+//
+//  Created by 三浦知明 on 2025/02/01.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubActionSampleTests/GithubActionSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionSampleTests.swift
+//  GithubActionSampleTests
+//
+//  Created by 三浦知明 on 2025/02/01.
+//
+
+import Testing
+@testable import GithubActionSample
+
+struct GithubActionSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
